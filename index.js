4937123a8fcc38c61c3c14fa20ca477682d071b7
RuzimbayevDior <ruzimbayevjs@gmail.com>
2024-11-28 19:05:29 +0200

    added BOM Dior

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ elForm.addEventListener("submit", function (evt) {
       id: todos[todos.length - 1]?.id + 1 || 0,
       name: inputvalue,
       isCompleted: false,
-      time: `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
+      time: `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`,
+      isShifr: false
     }
     todos.push(newTodo)
     elInput.value = ""
@@ -31,6 +32,11 @@ function renderTodos(arr, htmlElement) {
     const input = document.createElement("input")
     const time = document.createElement("p")
     const button = document.createElement("button")
+    const buttons = document.createElement("button")
+    const buttonds = document.createElement("button")
+    
+    
+    
 
     li.textContent = item.name
     button.textContent = "delete"
@@ -40,6 +46,12 @@ function renderTodos(arr, htmlElement) {
     button.classList.add("todo-btn")
     input.dataset.editTodoInputDataset = item.id
     input.classList.add("todo-input")
+    buttons.dataset.shifrTodoBtnDataset = item.id
+    buttons.classList.add("shifrlash")
+    buttons.textContent = "Shifrlash"
+    buttonds.dataset.deshifrTodoBtnDataset = item.id
+    buttonds.classList.add("deshifrlash")
+    buttonds.textContent = "Deshifrlash"
 
     if(item.isCompleted) {
       input.checked = true
@@ -53,6 +65,9 @@ function renderTodos(arr, htmlElement) {
     li.appendChild(input)
     li.appendChild(time)
     li.appendChild(button)
+    li.appendChild(buttons)
+    li.appendChild(buttonds)
+
     htmlElement.appendChild(li)
   })
 }
@@ -72,8 +87,38 @@ elList.addEventListener("click", function (evt) {
     foundedTodo.isCompleted = !foundedTodo.isCompleted
     elList.innerHTML = null
     renderTodos(todos, elList)
+  }else if(evt.target.matches(".shifrlash")){
+    const foundedDataset = evt.target.dataset.shifrTodoBtnDataset * 1
+    const foundedTodo = todos.find((item) => item.id === foundedDataset)
+    if (foundedTodo.isShifr === false) {
+      foundedTodo.name = window.atob(foundedTodo.name)
+    foundedTodo.isShifr = !foundedTodo.isShifr
+    }
+    elList.innerHTML = null
+    renderTodos(todos, elList)
+  }else if (evt.target.matches(".deshifrlash")) {
+    const foundedDataset = evt.target.dataset.deshifrTodoBtnDataset * 1
+    const foundedTodo = todos.find((item) => item.id === foundedDataset)
+    if (foundedTodo.isShifr === true) {
+      foundedTodo.name = window.btoa(foundedTodo.name)
+      foundedTodo.isShifr = !foundedTodo.isShifr
+    }
+    elList.innerHTML = null
+    renderTodos(todos, elList)
   }
+  
 })
 
+const elP = document.querySelector(".battery")
+
+window.navigator.getBattery().then((battery) => {
+  elP.textContent = `Battery Level: ${battery.level * 100}%`
+  elP.style.backgroundColor =
+    battery.level > 0.75? "green" : battery.level > 0.5? "yellow" : "red"
+})
 
 ////////// ended DOM
+
+
+
+
